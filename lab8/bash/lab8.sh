ef6aff80a5a71bed705a53b0de05e38e7c43d5cc
Liam Tolbert <treblotmail@gmail.com>
2024-05-01 03:41:16 +0000

    bash corrections

--- lab8/bash/lab8.sh
+++ lab8/bash/lab8.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Download the JSON data (assuming jq is installed)
 json_data=$(curl 'https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85' | jq .)
 
-# Extract the first six characters of each receiptTime value
 receipt_times=$(echo "$json_data" | jq '.[].receiptTime'  | head -n 6)
 
-# Print the receipt times
 echo "$receipt_times"
